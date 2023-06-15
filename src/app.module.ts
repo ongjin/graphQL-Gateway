738ff731a9977389684305bd222501a164e50121
ongjin <dydwls140@naver.com>
2023-06-15 17:41:41 +0900

    서버 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,41 @@ import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
-const map = {
-    asd:{ name: 'astemsapi', url: 'http://localhost:3030/graphql' },
-}
+const supergraphContext = ({ req }) => {
+    // Determine the subgraph based on the request information
+    let subgraph: any;
+
+    if (req.headers['x-subgraph'] === 'Member') {
+        subgraph = 'Member';
+    } else {
+        subgraph = 'astems';
+    }
+
+    // Return the subgraph information as part of the context
+    return { subgraph };
+};
+
+const supergraphGatewayConfig = {
+    supergraphSdl: new IntrospectAndCompose({
+        subgraphs: [
+            { name: 'Member', url: 'http://localhost:3030/graphql' },
+            // { name: 'astems', url: 'http://www.astems.co.kr:15000/graphql' },
+        ],
+    }),
+    buildService: ({ url, subgraph }) => {
+        console.log('subgraph', subgraph);
+        
+        return new RemoteGraphQLDataSource({
+            url,
+            willSendRequest({ request, context }) {
+                // Set the 'x-subgraph' header to specify the subgraph
+                request.http.headers.set('x-subgraph', subgraph);
+                request.http.headers.set('Authorization', context?.['headers']?.['authorization']);
+                request.http.headers.set('Content-Type', 'application/json');
+            },
+        });
+    },
+};
 
 @Module({
     imports: [
@@ -21,27 +53,20 @@ const map = {
             gateway: {
                 supergraphSdl: new IntrospectAndCompose({
                     subgraphs: [
-                        { name: 'Member', url: 'http://localhost:3030/graphql' },
-                        // { name: 'astems', url: 'http://www.astems.co.kr:15000/graphql' },
+                        // { name: 'Member', url: 'http://localhost:3030/graphql' },
+                        { name: 'astems', url: 'http://www.astems.co.kr:15000/graphql' },
                     ],
                 }),
 
-                buildService: ({ url }) => {
+                buildService: ({ url, name }) => {
+                    console.log('name', name, 'url', url);
+                    
                     return new RemoteGraphQLDataSource({
                         url,
-                        // willSendRequest({ request, context }) {
-                        //     request.http.headers.set(
-                        //         'authorization',
-                        //         context?.['headers']?.['authorization'],
-                        //     );
-                        //     request.http.headers.set(
-                        //         'Content-Type',
-                        //         'application/json'
-                        //     );
-                        // },
                         willSendRequest({ request, context }) {
-                            request.http.headers.set('authorization', context?.['headers']?.['authorization']);
-                            // request.http.headers.set('Content-Type', 'application/json');
+                            // request.http.headers.set('Authorization', context?.headers?.authorization);
+                            request.http.headers.set('Authorization', context?.['headers']?.['authorization'] || '');
+                            request.http.headers.set('Content-Type', 'application/json');
                         },
                     });
                 },
@@ -49,6 +74,7 @@ const map = {
         }),
     ]
 })
+
 
 
 
